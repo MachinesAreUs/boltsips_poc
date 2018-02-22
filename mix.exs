e7f1c1b75a1930910dc7faf6a81385ca3bdcb388
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2018-02-22 17:29:09 -0600

    updated bolt_sips to master (ea5e1ae3295700a9f9b0324d26d953845da1705)

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule BoltsipsPoc.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:bolt_sips, "~> 0.4"}
+      #{:bolt_sips, "~> 0.4"}
+      {:bolt_sips, git: "git@github.com:florinpatrascu/bolt_sips.git"}
     ]
   end
 end
